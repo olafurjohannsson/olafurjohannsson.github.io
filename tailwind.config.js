5fe6084dea88482bc4dbfc3430fbb0fa2abd6fad
Ólafur Aron <olafurjohannss@gmail.com>
2024-08-02 20:21:55 +0000

    update

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      'primary-blue': '#264653',
+      'primary-green': '#2a9d8f',
+      'primary-yellow': '#e9c46a',
+      'primary-orange': '#f4a261',
+      'primary-red': '#e76f51',
+      ...colors
+    },
+    extend: {
+      animation: {
+       'bounce-slow': 'bounce 2s infinite',
+      }
+    },
+  },
+  plugins: [],
+}
+
